d03dd9bb690581a87f7713ca1e1e15b54ba54bb3
joalisson <joalissonborges94@gmail.com>
2023-06-20 23:50:02 -0300

    feat: add login view

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
-module.export = {
+module.exports = {
     mode: 'development',
     entry: './src/main/index.tsx',
     output: {
@@ -10,15 +10,40 @@ module.export = {
         filename: 'bundle.js'
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
+        extensions: ['.ts', '.tsx', '.js', 'scss'],
         alias: {
             '@':path.join(__dirname, 'src')
         }
     },
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    },          {
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    },
     devServer: {
-        contentBase: './public',
-        writeToDisk: true,
-        historyApiFallback: true
+        devMiddleware: {
+          writeToDisk: true,
+        }
     },
     externals: {
         react: 'React',
